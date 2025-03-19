f6e40b1093e5e82699c2a1c914df1c0d0a04db41
0mShankar <omshankar9798@gmail.com>
2025-03-20 01:11:44 +0530

    production dir fixed

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app=express();
 
 
 const PORT=process.env.PORT || 5000;
-const _dirname=path.resolve();
+const __dirname=path.resolve();
 
 app.use(cors({
     origin:"http://localhost:5173",
